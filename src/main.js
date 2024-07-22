6d85af634c0d35f277dc6fe8c2a8b086b1952f35
tecinfoifc <luansilva250807@gmail.com>
2024-07-22 17:11:36 -0300

    instalation axios and configution axios to main.js

--- src/main.js
+++ src/main.js
@@ -2,12 +2,13 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import axios from 'axios'
+import '../src/plugins/axios'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-
+app.use(axios)
 app.use(createPinia())
 app.use(router)
 
