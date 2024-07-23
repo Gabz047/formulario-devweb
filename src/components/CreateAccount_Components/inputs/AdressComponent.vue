22471b652d042aaad9ff0c1a368a8a60d0dc58df
Gabz047 <gabe1612mail@gmail.com>
2024-07-23 15:30:35 -0300

    Created CreateAccount Component and separeted it in subcomponents. Defined store

---
+++ src/components/CreateAccount_Components/inputs/AdressComponent.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue';
+const isTyping = ref(false)
+</script>
+<template>
+  <span class="box-inputs">
+    <div class="box-inputs-img">
+      <i class="mdi mdi-home"></i>
+    </div>
+    <div class="box-inputs-content">
+      <label>
+        <div class="box-inputs-tittle" @click="isTyping = !isTyping"><p>Adress</p></div>
+        <input type="text" :class="isTyping ? 'activeOn' : 'activeOff'" />
+      </label>
+    </div>
+  </span>
+</template>
---
+++ src/components/CreateAccount_Components/inputs/EmailComponent.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue';
+const isTyping = ref(false)
+</script>
+<template>
+  <span class="box-inputs">
+    <div class="box-inputs-img">
+      <i class="mdi mdi-email"></i>
+    </div>
+    <div class="box-inputs-content">
+      <label>
+        <div class="box-inputs-tittle" @click="isTyping = !isTyping"><p>Email</p></div>
+        <input type="email" :class="isTyping ? 'activeOn' : 'activeOff'" />
+      </label>
+    </div>
+  </span>
+</template>
